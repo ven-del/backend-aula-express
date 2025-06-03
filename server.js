61fcefcf2dc8e7d66f2f2ca53c7d7865d45227bf
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-03 09:29:33 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const HOST = '127.0.0.1';
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor rodando em: http>//${HOST}:${PORT}}`)
+})
